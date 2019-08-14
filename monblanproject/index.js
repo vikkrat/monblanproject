76fdc193c264a54077e56166b876cfd90c9aa731
Viktoriia Kratser <poshta.victoria@gmail.com>
2019-08-14 18:56:04 +0300

    Final version

---
+++ monblanproject/index.js
@@ -0,0 +1,8 @@
+// ---------------------Imports--------------------------
+import './scss/output.scss';
+$('#fromDate').datepicker({
+  dateFormat: 'dd-mm-yy',
+});
+$('#toDate').datepicker({
+  dateFormat: 'dd-mm-yy',
+});
